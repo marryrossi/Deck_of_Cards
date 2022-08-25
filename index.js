e9f9c69a509f21643dc5868b957dec4af93ec9c6
Mary Rossi <maryrossi106@gmail.com>
2022-08-25 18:42:17 -0500

    rendering

--- index.js
+++ index.js
@@ -8,43 +8,48 @@
 let globalDeck; // Defining global variable with null value
 
 
+
 // Make an ajax request to api to get a new deck
 async function newDeck() {
-    console.log("newDeck")
+    // console.log("newDeck")
     const deck = await $.ajax('https://www.deckofcardsapi.com/api/deck/new/');
-
+    $cardSelection.innerHTML = "";
     globalDeck = deck;
+    console.log(deck); 
+
+    $remainingCardsDiv[0].innerText = "Remaining Cards: " + deck.remaining;
     return deck;
 };
 
 async function dealCards() {
-    console.log("dealCard")
-    const cards = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${globalDeck.deck_id}/draw/?count=2`)
-
-    $cardSelection = $('#cards-container');
+    // console.log("dealCard")
+    const data = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${globalDeck.deck_id}/draw/?count=2`)
+    console.log(data)
+    $cardSelection.innerHTML = "";
     // Using a for-loop for each card
-    for (let i=0; i < cards.remaining; i++) {
-        $card = document.createElement('div'); // Creating a new html element
-        $card.innerText = "card"; // Adding data from the card into the element
-        $card.className = "card"; 
-    
-        $cardSelection.append($card); // Inserting the element into the DOM
+    for (let i=0; i < data.cards.length; i++) {
+        let $cardImg = document.createElement('img'); // Creating a new html element
+        $cardImg.src = data.cards[i].image; // Adding data from the card into the element
+        
+        $cardSelection.append($cardImg); // Inserting the element into the DOM
     }
 
-    console.log(cards);
-    return cards;
+    $remainingCardsDiv[0].innerText = "Remaining Cards: " + data.remaining;
+    return data;
 };
 
-// async function shuffleCards(deckId) {
-//     console.log("deckId:", deckId)
-//     const cardsShuffled = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
+async function shuffleCards(deckId) {
+    // console.log("deckId:", deckId)
+    const cardsShuffled = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${globalDeck.deck_id}/shuffle/`)
 
-//     return cardsShuffled;
-// };
+    return cardsShuffled;
+};
 
 // Elements reference
 const $newDeckBtn = $('#new-deck-btn');
 const $dealCardsBtn = $('#deal-cards-btn');
+const $remainingCardsDiv = $('#remaining-cards');
+const $cardSelection = document.querySelector('#cards-container');
 
 // Event listener
 $newDeckBtn.on('click', newDeck);
@@ -52,3 +57,25 @@ $dealCardsBtn.on('click', dealCards);
 
 
 
+
+
+// function renderDeck(deck)
+// {
+//       document.getElementById("deck").innerHTML = "";
+
+// 	for(let i = 0; i < deck.length; i++)
+// 	{
+// 		let card = document.createElement("div");
+// 		let value = document.createElement("div");
+// 		let suit = document.createElement("div");
+// 		card.className = "card";
+// 		value.className = "value";
+// 		suit.className = "suit " + deck[i].Suit;
+
+// 		value.innerHTML = deck[i].Value;
+// 		card.appendChild(value);
+// 		card.appendChild(suit);
+
+// 		document.getElementById("deck").appendChild(card);
+// 	};
+// };
