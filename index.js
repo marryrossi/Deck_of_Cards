f40585744babef28d929203e6fb929db768426a2
Mary Rossi <maryrossi106@gmail.com>
2022-08-25 23:54:36 -0500

    comments/references

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
-let globalDeck; // Defining global variable with null value
+let globalDeck; // Defining variable
 
 
-
-// Make an ajax request to api to get a new deck
+// Making an ajax request to api to get a new deck. Referred to Mozilla async functions 
 async function newDeck() {
     // console.log("newDeck")
     const deck = await $.ajax('https://www.deckofcardsapi.com/api/deck/new/');
-    $cardSelection.innerHTML = "";
+    $cardSelection.innerHTML = ""; // this will clears the cards
     globalDeck = deck;
     console.log(deck); 
 
-    $remainingCardsDiv[0].innerText = "Remaining Cards: " + deck.remaining;
+    $remainingCardsDiv[0].innerText = "Remaining Cards: " + deck.remaining; // displays 52 cards remaining 
     return deck;
 };
 
+// Making ajax request to draw 2 cards
 async function dealCards() {
     // console.log("dealCard")
     const data = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${globalDeck.deck_id}/draw/?count=2`)
     console.log(data)
-    $cardSelection.innerHTML = "";
+    $cardSelection.innerHTML = ""; // clear cards
     // Using a for-loop for each card
     for (let i=0; i < data.cards.length; i++) {
-        let $cardImg = document.createElement('img'); // Creating a new html element
+        let $cardImg = document.createElement('img'); // Creating a new html element. Referred to w3 schools jQuery add elements
         $cardImg.src = data.cards[i].image; // Adding data from the card into the element
         
         $cardSelection.append($cardImg); // Inserting the element into the DOM
     }
 
-    $remainingCardsDiv[0].innerText = "Remaining Cards: " + data.remaining;
+    $remainingCardsDiv[0].innerText = "Remaining Cards: " + data.remaining; //displays cards remaining after draw
     return data;
-};
-
-async function shuffleCards(deckId) {
-    // console.log("deckId:", deckId)
-    const cardsShuffled = await $.ajax(`https://www.deckofcardsapi.com/api/deck/${globalDeck.deck_id}/shuffle/`)
-
-    return cardsShuffled;
 };
 
 // Elements reference
